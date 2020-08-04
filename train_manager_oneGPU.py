9adc51027571b3ee911139c84996dc03cc0940c4
Alone <593133977@qq.com>
2020-08-04 19:39:39 +0800

    Todo:CAM visial

--- train_manager_oneGPU.py
+++ train_manager_oneGPU.py
@@ -9,12 +9,12 @@ import argparse
 from torch.utils.data import DataLoader
 from utils import *
 import sys
-from sklearn.metrics import precision_score, recall_score,average_precision_score
+from sklearn.metrics import precision_score, recall_score, average_precision_score
 from tqdm import tqdm
 import os
 import torch.nn.functional as F
 import utils
-
+from tensorboardX import SummaryWriter
 
 parser = argparse.ArgumentParser("VOC_2012")
 parser.add_argument('--workers', type=int, default=4, help='number of workers to load dataset')
@@ -28,7 +28,8 @@ parser.add_argument('--report_freq', type=float, default=50, help='report freque
 parser.add_argument('--epochs', type=int, default=60, help='num of training epochs')
 # parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
 # parser.add_argument('--layers', type=int, default=8, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='./saved_models', help='path to save the model')
+parser.add_argument('--result_path', type=str, default='./saved_models', help='path to save the model')
+parser.add_argument('--exp_name', type=str, default='exp_temp', help='folder under result_path to save this run result')
 # parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 # parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
 # parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
@@ -36,20 +37,26 @@ parser.add_argument('--save', type=str, default='./', help='experiment name')
 # parser.add_argument('--seed', type=int, default=2, help='random seed')
 # parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
 # parser.add_argument('--unrolled', action='store_true', default=False, help='use one-step unrolled validation loss')
-parser.add_argument('--base_arch_lr', type=float, default=3e-3, help='learning rate for arch encoding')
+parser.add_argument('--base_arch_lr', type=float, default=6e-3, help='learning rate for arch encoding')
 parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
 parser.add_argument('--begin', type=int, default=6, help='batch size')
 parser.add_argument('--val_frequency', type=int, default=5, help='val frequency')
-parser.add_argument('--gpus',type=str,default='0')
+parser.add_argument('--tensorboard_frequency', type=int, default=50,
+                    help='every x times to save in tensorboardX during training')
+parser.add_argument('--gpus', type=str, default='0')
 args = parser.parse_args()
 
+savepath_for_this_exp = os.path.join(args.result_path, args.exp_name)
+if not os.path.exists(savepath_for_this_exp):
+    os.makedirs(savepath_for_this_exp)
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format=log_format, datefmt='%m/%d %I:%M:%S %p')
-fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+fh = logging.FileHandler(os.path.join(savepath_for_this_exp, 'log.txt'))
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
+writer = SummaryWriter(log_dir=savepath_for_this_exp)
 
 
 def main():
@@ -65,10 +72,10 @@ def main():
 
     model = trapezoid_supernet(max_scale=4, num_layers=12).cuda()
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size)
-    val_loader_2 = DataLoader(val_dataset, batch_size=args.batch_size*100)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, pin_memory=True, num_workers=5)
+    val_loader_2 = DataLoader(val_dataset, batch_size=args.batch_size * 100, pin_memory=True, num_workers=5)
 
-    criterion = nn.MultiLabelSoftMarginLoss().cuda()
+    criterion = nn.BCEWithLogitsLoss().cuda()
     optimizer = torch.optim.SGD(model.weight_parameters(),
                                 init_lr,
                                 momentum=args.momentum,
@@ -76,21 +83,15 @@ def main():
     optimizer_a = torch.optim.Adam(model.arch_parameters(), lr=init_arch_lr, betas=(0.9, 0.999),
                                    weight_decay=args.arch_weight_decay)
 
-
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, float(args.epochs), eta_min=args.learning_rate_min)
 
-
     best_mAP = 0
     for epoch in range(args.epochs):
-        current_lr = scheduler.get_lr()[0]
-        logging.info('Epoch: %d lr: %e', epoch, current_lr)
-        # genotype = model.genotype(
-        logging.info('genotype = %s', 'wait to complete')
-        # arch_param = model.arch_parameters()
+        current_lr = scheduler.get_last_lr()[0]
 
         mAP, obj = train(train_loader, val_loader, model, optimizer, optimizer_a, criterion,
-                                             current_lr,epoch)
+                         current_lr, epoch)
         scheduler.step()
 
         is_best = False
@@ -101,15 +102,19 @@ def main():
                 best_mAP = mAP
                 is_best = True
 
+            logging.info('Val Epoch: %d best_mAP: %3f mAP: %3f loss: %3f', epoch, best_mAP, mAP, obj)
+            writer.add_scalar('val/loss', obj, epoch)
+            writer.add_scalar('val/mAP', mAP, epoch)
+            writer.add_scalar('val/best_mAP', best_mAP, epoch)
+
         utils.save_checkpoint({
             'epoch': epoch,
             'state_dict': model.state_dict(),
             'best_acc_top1': best_mAP,
             'optimizer': optimizer.state_dict(),
             'optimizer_a': optimizer_a.state_dict(),
-        },is_best,args.model_path)
-
-
+        }, is_best, savepath_for_this_exp)
+        logging.info('Train Epoch: %d lr: %e loss: %3f', epoch, current_lr, obj)
 
 
 def train(train_loader, val_loader, model, optimizer, optimizer_a, criterion, lr, epoch):
@@ -118,6 +123,7 @@ def train(train_loader, val_loader, model, optimizer, optimizer_a, criterion, lr
     # recalls = AvgrageMeter()
     # precisions = AvgrageMeter()
     model.train()
+    val_loader = iter(val_loader)
 
     with tqdm(total=len(train_loader),
               desc='Train Epoch #{}'.format(epoch))as t:
@@ -129,11 +135,11 @@ def train(train_loader, val_loader, model, optimizer, optimizer_a, criterion, lr
             target = target.cuda()
 
             # get a random minibatch from the search queue with replacement
-            try:
-                input_search, target_search = next(valid_queue_iter)
-            except:
-                valid_queue_iter = iter(val_loader)
-                input_search, target_search = next(valid_queue_iter)
+            # try:
+            #     input_search, target_search = next(valid_queue_iter)
+            # except:
+            # valid_queue_iter = iter(val_loader)
+            input_search, target_search = next(val_loader)
             input_search = input_search.cuda(non_blocking=True)
             target_search = target_search.cuda(non_blocking=True)
 
@@ -144,36 +150,30 @@ def train(train_loader, val_loader, model, optimizer, optimizer_a, criterion, lr
                 loss_a.backward()
                 optimizer_a.step()
 
-
             optimizer.zero_grad()
             logits = model(input)
             loss = criterion(logits, target)
             loss.backward()
             optimizer.step()
-
-
-            # precision = precision_score(F.sigmoid(logits).cpu().data.numpy().round(),
-            #                             target.cpu().data.numpy().round(), average='micro')
-            # recall = recall_score(F.sigmoid(logits).cpu().data.numpy().round(),
-            #                       target.cpu().data.numpy().round(), average='micro')
-            mAP = utils.compute_mAP(target,F.sigmoid(logits))
             objs.update(loss.cpu().data.numpy(), n)
-            mAPs.update(mAP,n)
-            # precisions.update(precision, n)
-            # recalls.update(recall, n)
+            # mAP = utils.compute_mAP(target,F.sigmoid(logits))
+            # mAPs.update(mAP,n)
 
-            # if step % args.report_freq == 0:
-            #     logging.info('TRAIN Step: %03d Objs: %e precision: %f recall: %f', step, objs.avg, precisions.avg,
-            #                  recalls.avg)
+            if step % args.tensorboard_frequency == 0:
+                writer.add_scalar('Train/loss', objs.avg, epoch * len(train_loader) + step)
+                writer.add_scalar('Train/lr', lr, epoch * len(train_loader) + step)
+
             t.set_postfix({
                 'loss': objs.avg,
-                'mAP': mAPs.avg,
                 'lr': lr,
+                'mAP': 'wait until Val',
             })
             t.update(1)
-    return mAPs.avg, objs.avg
+        # return mAPs.avg, objs.avg
+        return None, objs.avg
 
-def infer(val_loader, model, criterion,epoch):
+
+def infer(val_loader, model, criterion, epoch):
     objs = AvgrageMeter()
     mAPs = AvgrageMeter()
     model.eval()
